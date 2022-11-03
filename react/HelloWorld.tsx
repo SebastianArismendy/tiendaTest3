b61d3ca937ccab97fee5358c06de14c968fafd29
Sebastian Arismendy Buitrago <sebastian.arismendy@pragma.com.co>
2022-11-02 23:37:54 -0500

    react components

---
+++ react/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1 className="fw9">Hello world</h1>
+        </div>
+    )
+}
+
+HelloWorld.displayName = "helloworld";
+
+export default HelloWorld;
